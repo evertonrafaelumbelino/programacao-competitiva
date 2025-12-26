3e9c613bb039e52be851a1dab3a9f824ec66ecaa
evertonrafaelumbelino <evertonrafaelumbelino@gmail.com>
2025-12-26 09:01:13 -0300

    problema media

---
+++ problemas-competicao/nivel-basico/media.cpp
@@ -0,0 +1,15 @@
+/*
+    Problema: Média inteira
+    Fonte: Neps Academy
+    Técnica utilizada: Variáveis e operadores aritméticos
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Lógica: Leio os valores de A e B e os guardo em variáveis, depois imprimo a média deles: (A+B)/2
+    int a, b;
+    cin >> a >> b;
+    cout << (a+b)/2;
+}
