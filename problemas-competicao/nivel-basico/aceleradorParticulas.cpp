644c645941eaf9623255ed3115ccb19d51be6850
evertonrafaelumbelino <evertonrafaelumbelino@gmail.com>
2025-12-27 08:05:22 -0300

    problema acelerador de particulas

---
+++ problemas-competicao/nivel-basico/aceleradorParticulas.cpp
@@ -0,0 +1,25 @@
+/*
+    Problema: Acelerador de partículas
+    Fonte: OBI 2020 - Nível 2 - Primeira fase
+    Técnica utilizada:
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    /*
+        Lógica:
+            Leio o valor em km percorrido pela partícula.
+            Subtraio esse valor por 3, tirando a quantia inicial do acelerador de partículas (que é padrão em todos os casos).
+            Depois divido o valor por 8, e pego o resto que sobrar.
+            Com esse resto, basta subtrair por 2, assim encontrando o sensor.
+            Resto 3: sensor 1 (3-2); resto 4, sensor 2 (4-2); resto 5: sensor 3 (5-2)
+    */
+    int d;
+    cin >> d;
+    d -= 3;
+    d %= 8;
+    cout << d-2;
+}
