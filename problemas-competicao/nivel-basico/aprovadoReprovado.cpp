9e3032c8df7952ba80f4ea65e208a0a0a7a65916
evertonrafaelumbelino <evertonrafaelumbelino@gmail.com>
2025-12-29 08:03:40 -0300

    ad logica problema aprovado ou reprovado

--- problemas-competicao/nivel-basico/aprovadoReprovado.cpp
+++ problemas-competicao/nivel-basico/aprovadoReprovado.cpp
@@ -7,6 +7,11 @@
 using namespace std;
 
 int main() {
+    /*
+        Lógica:
+            Leio os valores das duas notas enviadas (n1, n2) e calculo a média delas (m).
+            Uso condicionais para verificar se o aluno está aprovado (m>=7), de recuperação (m>=4 e m<7) ou se foi reprovado (m<4).
+    */
     float n1, n2, m;
     cin >> n1 >> n2;
     m = (n1+n2)/2;
